a0b14fcfe3a1c59f24884dbb391c78bb023856ac
Keith Kabza Personal <keith@visualgov.com>
2025-04-18 13:17:48 -0400

    feat(entity): add index file for easier entity exports

---
+++ src/models/entities/index.ts
@@ -0,0 +1,6 @@
+// Export all entities
+export { Auction } from './auction.entity';
+export { County } from './county.entity';
+export { Property } from './property.entity';
+export { Certificate } from './certificate.entity';
+export { User } from './user.entity';
